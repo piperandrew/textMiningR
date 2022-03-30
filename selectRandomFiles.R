196a43015474568a3c9a6cea130e1a3d1153ed92
Andrew Piper <akpiper11@gmail.com>
2022-03-30 10:45:51 -0400

    Update

--- selectRandomFiles.R
+++ selectRandomFiles.R
@@ -8,27 +8,19 @@
 
 #set working director where your source texts are located
 #source.texts<-c("~/Desktop/LordOfTheRings_FanFic")
-source.texts<-c("/Volumes/Samsung_T5/Peter_Stuff/FANFICTION/FINAL_DATASET/texts")
-target.texts<-c("~/Desktop/FanFic")
+target.texts<-c("~/Desktop/LordOfTheRings_FanFic_Sample")
 setwd(source.texts)
 
 #get list of files
 fn<-list.files()
 
 #sample n files
-n<-50
+n<-500
 fn.s<-sample(fn, n)
-
-#or get list of files in table
-t<-read.csv("")
-final.df1$filename<-paste(final.df1$id, ".txt", sep="")
-fn.s<-fn[fn %in% final.df1$filename]
 
 #loop through ingest and write
 for (i in 1:length(fn.s)){
-  
   print(i)
-  
   #setwd to source texts directory
   setwd(source.texts)
   #ingest
@@ -51,3 +43,4 @@ for (i in 1:length(fn.s)){
   #write
   write(work.all, file=fn.s[i])
 }
+
