5952808522d86c3411a414a462094d2098398c82
Andrew Piper <akpiper11@gmail.com>
2024-06-07 18:27:34 -0400

    Update

--- bookNLP_selectRandomSentencesWithCharacters.R
+++ bookNLP_selectRandomSentencesWithCharacters.R
@@ -37,20 +37,24 @@ file.sub<-unique(file.sub)
 #establish parameters
 
 #sample n documents
-n<-2
+n<-20
 fn.s<-sample(file.sub, n)
 #fn.s<-file.sub
 
 #set number of context sentences prior to target sentence with character
-c=4
+c=1
 #set number of following sentences (c+s+1 = total sentences per passage)
-s=1
+s=3
+#set max passage length in words
+mx=125
+#set number of repetitions per character per passage
+max.repeat<-2
 
 #select number of passages to extract per book (of s sequential sentences)
 pass<-1
 
-#subset filenames
-filenames2<-filenames[sub("\\..*$", "", filenames) %in% fn.s]
+#supersense category to sample from
+cat<-c("verb.emotion", "noun.feeling")
 
 #create empty final table
 final.df<-NULL
@@ -65,6 +69,9 @@ for (i in 1:length(fn.s)){
   
   #load dialogue table
   quotes.df<-read.csv(paste(fn.s, ".quotes", sep="")[i], quote="", sep="\t")
+  
+  #load supersense table
+  super.df<-read.csv(paste(fn.s, ".supersense", sep="")[i], quote="", sep="\t")
   
   #load character table
   char.df<-read.csv(paste(fn.s, ".entities", sep="")[i], quote="", sep="\t")
@@ -93,14 +100,20 @@ for (i in 1:length(fn.s)){
   #keep only sentences with these characters
   tokens.df2<-tokens.df1[tokens.df1$sentence_ID %in% tokens.char$sentence_ID,]
   
-  #further remove sentences in dialogue
-  
+  ### remove sentences in dialogue ###
   #get all sentences with dialogue
-  sequences <- unlist(mapply(seq, from=quotes.df$quote_start, to=quotes.df$quote_end))
-  dia.df<-tokens.df[tokens.df$token_ID_within_document %in% sequences,]
-
+  #sequences <- unlist(mapply(seq, from=quotes.df$quote_start, to=quotes.df$quote_end))
+  #dia.df<-tokens.df[tokens.df$token_ID_within_document %in% sequences,]
   #subset tokens table by removing these sentences
-  tokens.df2<-tokens.df2[!tokens.df2$sentence_ID %in% dia.df$sentence_ID,]
+  #tokens.df2<-tokens.df2[!tokens.df2$sentence_ID %in% dia.df$sentence_ID,]
+  
+  ### subset by supersense ###
+  #subset by target supersense(s)
+  super.df<-super.df[super.df$supersense_category %in% cat,]
+  #get sentence IDs
+  super.sent<-tokens.df$sentence_ID[tokens.df$token_ID_within_document %in% super.df$start_token]
+  #subset tokens table by removing these sentences
+  tokens.df2<-tokens.df2[tokens.df2$sentence_ID %in% super.sent,]
   
   for (j in 1:pass){
     
@@ -110,14 +123,31 @@ for (i in 1:length(fn.s)){
       #start c sentences prior to target sentence and s sentences after
       samp.s<-seq(from=(start-c), to=(start+s), by=1)
       tokens.s<-tokens.df[tokens.df$sentence_ID %in% samp.s,]
+      
+      #### no dialogue condition ####
       #check if those sentences are in dialogue table, resample if so
-      if (length(which(unique(tokens.s$sentence_ID) %in% dia.df$sentence_ID)) == 0){
+      #if (length(which(unique(tokens.s$sentence_ID) %in% dia.df$sentence_ID)) == 0){
+      #  break
+      #}
+      
+      ### minimum character count condition ####
+      #get char.id
+      char.id<-char.df$COREF[char.df$start_token %in% tokens.df2$token_ID_within_document[tokens.df2$sentence_ID == start]][1]
+      #get all chars for that passage
+      char.sub<-char.df[char.df$start_token %in% tokens.s$token_ID_within_document,]
+      #table character Ids
+      char.list<-table(char.sub$COREF)
+      #subset by char.id
+      char.list<-char.list[names(char.list) == char.id]
+      #main character must appear at least 2 times
+      if (unname(char.list) > max.repeat & length(tokens.s) < mx){
         break
       }
     }
   
     #get character name
     character<-char.df$text[char.df$start_token %in% tokens.df2$token_ID_within_document[tokens.df2$sentence_ID == start]][1]
+    char.id<-char.df$COREF[char.df$start_token %in% tokens.df2$token_ID_within_document[tokens.df2$sentence_ID == start]][1]
     
     #combine into a single passage
     p<-paste(tokens.s$word, sep=" ", collapse=" ")
@@ -133,14 +163,17 @@ for (i in 1:length(fn.s)){
   }
 }
 
-final2<-rbind(final2, final.df)
-
-setwd("/Users/akpiper/Research/Character Cognition")
-write.csv(final.df, file="Test01.csv", row.names = F)
-
 #add id to each row
-library(stringi)
-final.df$ID<-paste(stri_rand_strings(nrow(final.df), 6, pattern = "[A-Za-z0-9]"), ".txt", sep="")
+#library(stringi)
+#final.df$ID<-paste(stri_rand_strings(nrow(final.df), 6, pattern = "[A-Za-z0-9]"), ".txt", sep="")
+
+#create new column for Lighttag
+final2<-final.df
+final2$combined<-paste(final2$character, final2$p, sep="\n\n")
+
+#save
+write.csv(final.df, file="Emotions02.csv", row.names = F)
+
 
 #write texts to files
 setwd("/Users/akpiper/Research/Character Cognition/CharacterCognition_Test")
@@ -148,10 +181,6 @@ setwd("/Users/akpiper/Research/Character Cognition/CharacterCognition_Test")
 for (i in 1:nrow(final.df)){
   write(final.df[i,2], file=final.df$ID[i])
 }
-
-#create new column for Lighttag
-test<-final2
-final2$combined<-paste(final2$character, final2$p, sep="\n\n")
 
 
 
